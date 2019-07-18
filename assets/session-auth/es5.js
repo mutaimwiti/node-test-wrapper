d472702490b9de5da17013009e8c1b5450558dc6
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-07-18 14:41:08 +0300

    (feat): create supertest wrapper boilerplate
    - session-auth - es5 and es6
    - token-auth - es5 and es6
    - basic-auth - es5 and es6
    - no-auth - es5 and es6

---
+++ assets/session-auth/es5.js
@@ -0,0 +1,109 @@
+const supertest = require('supertest');
+const appDef = require('../path/to/your/app');
+
+const app = {
+  /**
+   * @private
+   */
+  request: supertest(appDef),
+
+  /**
+   * @private
+   */
+  cookie: null,
+
+  /**
+   * When this method is called it generates a session cookie based on the details of
+   * the user that is supplied. This can be achieved by making a login request to the
+   * app and obtaining the cookie from the response headers. On a role-based auth
+   * system you might consider having an extra optional argument to specify the
+   * role or permissions of the user. This will allow you to assign the random
+   * user the required permission(s).
+   *
+   * @param user
+   * @returns {Promise<void>}
+   */
+  async login(user) {
+    // add logic to generate auth cookie here - ideally this will involve invoking
+    // the login endpoint and extracting "set-cookie" from the response headers
+    // replace this with the actual implementation
+    this.cookie = 'generated-cookie';
+  },
+
+  /**
+   * When this method is called it generates a session cookie based on the details of
+   * the random user. This can be achieved by making a login request to the app and
+   * obtaining the cookie from the response headers. On a role-based auth system
+   * you might consider having an optional argument to specify the role or
+   * permissions of the user. This will allow you to assign the random
+   * user the required permission(s).
+   *
+   * @returns {Promise<void>}
+   */
+  async loginRandom() {
+    // create a random user - entirely up to your persistence system
+    // add logic to generate auth cookie here - ideally this will involve invoking
+    // the login endpoint and extracting "set-cookie" from the response headers
+    // replace this with the actual implementation
+    this.cookie = 'generated-cookie';
+  },
+
+  /**
+   * When this method is called it clears authentication data on the wrapper.
+   * This means that calls to http wrapper methods generate request objects
+   * that don't have the authentication header.
+   */
+  logout() {
+    // remove authentication cookie
+    this.cookie = null;
+  },
+
+  /**
+   * This method contains pre-request logic. It is executed by all http
+   * wrapper methods i.e. get, post, ... , to alter the request object.
+   * It's main use is to set the authentication header.
+   *
+   * @param request
+   * @returns {*}
+   * @private
+   */
+  preRequest(request) {
+    // add pre-request logic
+    // set the authorization header
+    return this.cookie ? request.set('cookie', this.cookie) : request;
+  },
+
+  // http wrapper methods
+  // get(), post(), put(), patch(), delete()
+  // you can add more methods offered by supertest
+
+  get(url) {
+    const req = this.request.get(url);
+
+    return this.preRequest(req);
+  },
+
+  post(url) {
+    const req = this.request.post(url);
+
+    return this.preRequest(req);
+  },
+
+  put(url) {
+    const req = this.request.put(url);
+
+    return this.preRequest(req);
+  },
+
+  patch(url) {
+    const req = this.request.patch(url);
+
+    return this.preRequest(req);
+  },
+
+  delete(url) {
+    const req = this.request.delete(url);
+
+    return this.preRequest(req);
+  },
+};
---
+++ assets/token-auth/es6.js
@@ -0,0 +1,108 @@
+import supertest from 'supertest';
+import appDef from '../path/to/your/app';
+
+class app {
+  /**
+   * @private
+   */
+  static request = supertest(appDef);
+
+  /**
+   * @private
+   */
+  static token = null;
+
+  /**
+   * When this method is called it generates an auth token based on the details of
+   * the user that is supplied. On a role-based auth system you might consider
+   * having an extra optional argument to specify the role or permissions of
+   * the user. This will allow you to assign the user the required
+   * permission(s).
+   *
+   * @param user
+   * @returns {Promise<void>}
+   */
+  static async login(user) {
+    // add logic to generate user authentication details here ...
+    // replace this with the actual implementation
+    this.token = "generated-auth-token";
+  }
+
+  /**
+   * When this method is called it generates an auth token based on the details of
+   * a randomly generated user. On a role-based auth system you might consider
+   * having an optional argument to specify the role or permissions of the
+   * user. This will allow you to assign the random user the required
+   * permission(s).
+   *
+   * @returns {Promise<void>}
+   */
+  static async loginRandom() {
+    // create a random user - entirely up to your persistence system
+    // add logic to generate user authentication token here ...
+    // replace this with the actual implementation
+    this.token = "generated-auth-token";
+  }
+
+  /**
+   * When this method is called it clears authentication data on the wrapper.
+   * This means that calls to http wrapper methods generate request objects
+   * that don't have the authentication header.
+   */
+  static logout() {
+    // remove authentication token
+    this.token = null;
+  }
+
+  /**
+   * This method contains pre-request logic. It is executed by all http
+   * wrapper methods i.e. get, post, ... , to alter the request object.
+   * It's main use is to set the authentication header.
+   *
+   * @param request
+   * @returns {*}
+   * @private
+   */
+  static preRequest(request) {
+    // add pre-request logic
+    // set the authentication header
+    // set whatever header your system uses for authentication
+    return this.token ? request.set('authorization', this.token) : request;
+  }
+
+  // http wrapper methods
+  // get(), post(), put(), patch(), delete()
+  // you can add more methods offered by supertest
+
+  static get(url) {
+    const req = this.request.get(url);
+
+    return this.preRequest(req);
+  }
+
+  static post(url) {
+    const req = this.request.post(url);
+
+    return this.preRequest(req);
+  }
+
+  static put(url) {
+    const req = this.request.put(url);
+
+    return this.preRequest(req);
+  }
+
+  static patch(url) {
+    const req = this.request.patch(url);
+
+    return this.preRequest(req);
+  }
+
+  static delete(url) {
+    const req = this.request.delete(url);
+
+    return this.preRequest(req);
+  }
+}
+
+export default app;
