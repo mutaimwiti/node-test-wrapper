3d46fbbefa084a9039f04b76015e80c99dc89c6a
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-11-13 13:49:58 +0300

    (chore): create basic-auth es6 example express app with tests

---
+++ examples/basic-auth/es6/__mock__.js
@@ -0,0 +1,13 @@
+const mockUsers = [
+  {
+    username: 'admin',
+    password: 'admin_pass',
+  },
+  {
+    username: 'joe',
+    password: 'joe_pass',
+  },
+];
+
+// eslint-disable-next-line
+export { mockUsers };
---
+++ examples/basic-auth/es6/test/base.spec.js
@@ -0,0 +1,10 @@
+import app from './utils/app';
+
+describe('App', function() {
+  it('should list all articles', function(done) {
+    app.get('/').then(function({ body }) {
+      expect(body.message).toEqual('Welcome');
+      done();
+    });
+  });
+});
