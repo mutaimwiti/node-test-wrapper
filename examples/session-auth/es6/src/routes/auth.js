669afa5f4251602935d861d8138e98d18aa189c2
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-11-14 17:31:32 +0300

    (chore): move session-auth es6 find user logic to src/utils.js

--- examples/session-auth/es6/src/routes/auth.js
+++ examples/session-auth/es6/src/routes/auth.js
@@ -1,20 +1,13 @@
 import { Router } from 'express';
-import { mockUsers } from '../__mock__';
+import { findUser } from '../utils';
 
 const router = Router();
 
 router.post('/login', (req, res) => {
   if (!req.session.user) {
-    const data = {
-      name: req.body.username,
-      pass: req.body.password,
-    };
+    const data = req.body;
 
-    const found = mockUsers.find(
-      (user) => user.username === data.name && user.password === data.pass,
-    );
-
-    if (found) {
+    if (findUser(data)) {
       req.session.user = data;
       return res.status(201).json({ message: 'Logged in successfully' });
     }
