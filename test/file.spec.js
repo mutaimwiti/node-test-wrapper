40276127a43f4f39350178c59f1b2afbe24fd603
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-11-25 17:03:09 +0300

    (chore): test fileAvailable() file util function

--- test/file.spec.js
+++ test/file.spec.js
@@ -1,4 +1,4 @@
-import { jsExtensionPath } from '../src/utils/file';
+import { jsExtensionPath, fileAvailable } from '../src/utils/file';
 
 describe('utils/file.js', () => {
   describe('jsExtensionPath()', () => {
@@ -12,6 +12,48 @@ describe('utils/file.js', () => {
 
     it('should append .js if it is not valid js extension', () => {
       expect(jsExtensionPath('testFile')).toEqual('testFile.js');
+    });
+  });
+
+  describe('fileAvailable()', () => {
+    it('should fail if is invalid file path', () => {
+      expect(fileAvailable('test/*app.js')).toEqual(
+        "'test/*app.js' is not a valid file path",
+      );
+    });
+
+    it('should fail if it ends with /', () => {
+      expect(fileAvailable('test/app.js/')).toEqual(
+        "'test/app.js/' is not a valid file path",
+      );
+    });
+
+    it('should fail if it ends with \\', () => {
+      expect(fileAvailable('test/app.js\\')).toEqual(
+        "'test/app.js\\' is not a valid file path",
+      );
+    });
+
+    it('should fail if it is an absolute path', () => {
+      expect(fileAvailable('/test/app.js')).toEqual(
+        'Specify a relative file path',
+      );
+    });
+
+    it('should fail if it is an existing directory', () => {
+      expect(fileAvailable('test/stubs/dir.js')).toEqual(
+        "'test/stubs/dir.js' is a directory",
+      );
+    });
+
+    it('should fail if it is an existing file', () => {
+      expect(fileAvailable('test/stubs/app.js')).toEqual(
+        "File 'test/stubs/app.js' already exists",
+      );
+    });
+
+    it('should return true if file path is available', () => {
+      expect(fileAvailable('tests/stubs/testFile.js')).toEqual(true);
     });
   });
 });
