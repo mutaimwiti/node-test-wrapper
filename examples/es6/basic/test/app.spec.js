c4c107504e06174f9c254bbd5243644ed71afeab
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-09 21:23:46 +0300

    (chore): create proper setup and teardown for mongodb

--- examples/es6/basic/test/app.spec.js
+++ examples/es6/basic/test/app.spec.js
@@ -2,9 +2,6 @@ import app from './testUtils/app';
 
 describe('App', function() {
   it('should show welcome message', function(done) {
-    app.get('/').then(function({ body }) {
-      expect(body.message).toEqual('Welcome');
-      done();
-    });
+    app.get('/').expect({ message: 'Welcome' }, done);
   });
 });
