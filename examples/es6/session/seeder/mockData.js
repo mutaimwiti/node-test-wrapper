ce4d1c3e45a9af8d676ef0e26e3fa93ec701d3d4
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-10 18:48:04 +0300

    (chore): add db seeder to es6 session-auth example

---
+++ examples/es6/session/seeder/mockData.js
@@ -0,0 +1,35 @@
+const mockUsers = [
+  {
+    username: 'admin',
+    password: 'admin_pass',
+  },
+  {
+    username: 'joe',
+    password: 'joe_pass',
+  },
+];
+
+const mockReports = [
+  {
+    title: 'lacus',
+    body: 'Pharetra sit amet aliquam id diam maecenas',
+  },
+  {
+    title: 'viverra',
+    body: 'Purus sit amet luctus venenatis lectus',
+  },
+];
+
+const mockArticles = [
+  {
+    title: 'viverra',
+    body: 'Ac orci phasellus egestas tellus',
+  },
+  {
+    title: 'volutpat',
+    body: 'Tortor posuere ac ut consequat semper viverra nam libero',
+  },
+];
+
+// eslint-disable-next-line
+export { mockUsers, mockReports, mockArticles };
