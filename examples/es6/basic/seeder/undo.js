e8a78fccdcb680eadb8a29c0abc93c11ef90f153
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-10 18:19:29 +0300

    (chore): move es6 basic auth db seed logic to project root
    - move db seed logic from src to project root

--- examples/es6/basic/seeder/undo.js
+++ examples/es6/basic/seeder/undo.js
@@ -1,9 +1,11 @@
-import { User } from '../models';
-import { connect, disconnect } from '../db';
+import { Article, Report, User } from '../src/models';
+import { connect, disconnect } from '../src/db';
 
 const undo = async () => {
   console.log('=============== Clearing database ===============');
   await User.deleteMany();
+  await Article.deleteMany();
+  await Report.deleteMany();
   console.log('============= Cleared successfully ==============');
   process.exit(0);
 };
