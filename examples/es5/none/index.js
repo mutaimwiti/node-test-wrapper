fc8533ba6c985007ab82c4351a2017433cb2376e
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-11 18:22:37 +0300

    (chore): add database connection to es5 example entry files

--- examples/es5/none/index.js
+++ examples/es5/none/index.js
@@ -1,5 +1,8 @@
 var app = require('./src/app');
+var connect = require('./src/db').connect;
 
-app.listen(3000, function() {
-  console.log('Listening on http://localhost:3000');
+connect(function() {
+  app.listen(3000, function() {
+    console.log('Listening on http://localhost:3000');
+  });
 });
