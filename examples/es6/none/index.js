f49d207077769a3464291ba305865840cc958436
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-11 18:16:56 +0300

    (chore): add database connection to es6 example entry files

--- examples/es6/none/index.js
+++ examples/es6/none/index.js
@@ -1,4 +1,9 @@
 import app from './src/app';
+import { connect } from './src/db';
+
+(async () => {
+  await connect();
+})();
 
 app.listen(3000, () => {
   console.log('Listening on http://localhost:3000');
